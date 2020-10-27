ba81e667473d08f3b18b0d0dbbb56832968e849c
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-27 11:49:45 -0600

    appedn 0 too decents of hrs

--- clocky-mc-clock-fac.js
+++ clocky-mc-clock-fac.js
@@ -18,19 +18,25 @@ let angle = 90
 // let angle = 360
 
 var whatTimeIsIt = function(angle) {
-	// Your code here
-	let res=""
-	let hrs=""
-	let mins="00"
-	if(angle==0){
-		hrs=12
-	}else{
+  // Your code here
+  let res = ""
+  let hrs = ""
+  let mins = "00"
+  if (angle == 0) {
+    hrs = 12
+  } else {
 
-		hrs = angle / 30
-	}
-	res=hrs+":"+mins
+    hrs = angle / 30
+  }
 
-	console.log(res);
+  if (hrs.toString().length < 2) {
+		hrs = "0"+hrs.toString()
+		console.log(hrs);
+  }
+
+
+
+  res = hrs + ":" + mins
   return res;
 }
 
@@ -38,39 +44,39 @@ var whatTimeIsIt = function(angle) {
 
 function clock_angles(hour, minute) {
 
-	// note: there are 360 degrees around the clock interface
+  // note: there are 360 degrees around the clock interface
 
-	// calculate the degree position of the hour hand
-	hourDegree = 30 * hour;
+  // calculate the degree position of the hour hand
+  hourDegree = 30 * hour;
 
-	// calculate how far the hour hand is from zero
-	if ( hourDegree < 180 ) {
-		hourDifference = hourDegree;
-	} else {
-		hourDifference = 360 - hourDegree;
-	}
+  // calculate how far the hour hand is from zero
+  if (hourDegree < 180) {
+    hourDifference = hourDegree;
+  } else {
+    hourDifference = 360 - hourDegree;
+  }
 
-	// calculate the degree position of the minute hand
-	// if the minute hand is on the 12 then the degree position should be 0
-	if ( minute == 60 ) {
-		minute = 0;
-	}
+  // calculate the degree position of the minute hand
+  // if the minute hand is on the 12 then the degree position should be 0
+  if (minute == 60) {
+    minute = 0;
+  }
 
-	// calculate the minutes based on the position
-	minuteDegree = 6 * minute;
+  // calculate the minutes based on the position
+  minuteDegree = 6 * minute;
 
-	// calculate how far the minute hand is from zero
-	if ( minuteDegree < 180 ) {
-		minuteDifference = minuteDegree;
-	} else {
-		minuteDifference = 360 - minuteDegree;
-	}
+  // calculate how far the minute hand is from zero
+  if (minuteDegree < 180) {
+    minuteDifference = minuteDegree;
+  } else {
+    minuteDifference = 360 - minuteDegree;
+  }
 
-	// calculate the difference between the hour and minute hand
-	handDifference = hourDifference + minuteDifference;
+  // calculate the difference between the hour and minute hand
+  handDifference = hourDifference + minuteDifference;
 
-	// return the difference
-	return handDifference;
+  // return the difference
+  return handDifference;
 }
 
 whatTimeIsIt(angle)
